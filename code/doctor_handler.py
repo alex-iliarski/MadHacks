d4a61e0a60b34e28f106aca89e88b987e4aa2aa6
Andrew <AndrewOMagnuson@gmail.com>
2023-03-04 16:09:06 -0600

    enum

---
+++ code/doctor_handler.py
@@ -0,0 +1,37 @@
+from enum import Enum
+
+Speciality = Enum(
+    "Speciality",
+    [
+        "CARDIOLOGIST",
+        "DENTIST",
+        "DERMATOLOGIST",
+        "ENDOCRINOLOGIST",
+        "GASTROENTEROLOGIST",
+        "GENERAL_PRACTITIONER",
+        "GYNECOLOGIST",
+        "HEMATOLOGIST",
+        "INFECTIOUS_DISEASES_SPECIALIST",
+        "NEPHROLOGIST",
+        "NEUROLOGIST",
+        "NEUROSURGEON",
+        "OBSTETRICIAN",
+        "ONCOLOGIST",
+        "OPHTHALMOLOGIST",
+        "ORTHOPEDIST",
+        "OTORHINOLARYNGOLOGIST",
+        "PEDIATRICIAN",
+        "PHYSIATRIST",
+        "PSYCHIATRIST",
+        "RADIOLOGIST",
+        "RHEUMATOLOGIST",
+        "SURGEON",
+        "UROLOGIST",
+    ],
+)
+
+Insurence = Enum("Insurence", ["ALINA_HEALTH", "ALLIANZ", "ASANATOARE"])
+
+Language = Enum("Language", ["ENGLISH", "FRENCH", "GERMAN", "ITALIAN", "SPANISH"])
+
+Gender = Enum("Gender", ["MALE", "FEMALE"])
