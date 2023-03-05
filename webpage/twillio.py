54a5cceb0547601dd718588e23595c69981fa4f1
Andrew <AndrewOMagnuson@gmail.com>
2023-03-04 21:15:09 -0600

    twillio!

--- webpage/twillio.py
+++ webpage/twillio.py
@@ -1,5 +1,23 @@
+from twilio.rest import Client
+from query import get_doc_by_id
+from dotenv import load_dotenv, find_dotenv
+import os
 
+load_dotenv(find_dotenv())
+
+sid = os.environ.get("TWILIO_ACOUNT_ID")
+token = os.environ.get("TWILIO_AUTH_TOKEN")
+
+client = Client(sid, token)
 
 
 def message_doc(doc_id, message):
-    print("TODO")
+    doctor = get_doc_by_id(doc_id)
+    client.messages.create(
+        body=message,
+        from_="+13159037882",
+        to="+1" + doctor["phone"],
+    )
+
+
+message_doc("6403ce94cfc8fce87221aab2", "Hello, this is a test message")
