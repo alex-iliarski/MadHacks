1b30a581126228e03025776f534629a2b8b470d0
Andrew <AndrewOMagnuson@gmail.com>
2023-03-04 22:42:06 -0600

    t

--- webpage/routes.py
+++ webpage/routes.py
@@ -8,8 +8,8 @@ from webpage.query import find_doctors, get_doc_by_id
 from flask_googlemaps import Map, GoogleMaps, icons
 
 
-@app.route('/')
-@app.route('/home')
+@app.route("/")
+@app.route("/home")
 def home():
     map = Map(
         identifier="map",
@@ -18,36 +18,37 @@ def home():
         zoom=12,
         style="height:800px;width:100%;margin:0;",
         markers=[
-          {
-             'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-             'lat': 37.4419,
-             'lng': -122.1419,
-             'infobox': "<p>Hello World</p>"
-          },
-          {
-             'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-             'lat': 37.4300,
-             'lng': -122.1400,
-             'infobox': "<p>Hello World from other place</p>"
-          }
-        ]
+            {
+                "icon": "http://maps.google.com/mapfiles/ms/icons/green-dot.png",
+                "lat": 37.4419,
+                "lng": -122.1419,
+                "infobox": "<p>Hello World</p>",
+            },
+            {
+                "icon": "http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
+                "lat": 37.4300,
+                "lng": -122.1400,
+                "infobox": "<p>Hello World from other place</p>",
+            },
+        ],
     )
-    return render_template('home.html', map=map)
+    return render_template("home.html", map=map)
 
-@app.route('/about')
+
+@app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template("about.html")
 
-@app.route('/contact')
+
+@app.route("/contact")
 def contact():
-    return render_template('contact.html')
+    return render_template("contact.html")
 
 
-@app.route('/docquery', methods=["GET", "POST"])
+@app.route("/docquery", methods=["GET", "POST"])
 def docquery():
     form = QueryForm()
 
-    
     if form.validate_on_submit():
         # # make query and redirect to results page
         query_zipcode = form.zipcode.data
@@ -65,28 +66,34 @@ def docquery():
         if query_gender == None:
             query_gender = ""
 
-        docs = find_doctors(query_zipcode, 
-                            within_miles=query_distance, 
-                            specialization=query_specialization, 
-                            years_of_experience=query_years_experience, 
-                            insurence=query_insurance,
-                            language=query_lang,
-                            gender=query_gender)
+        docs = find_doctors(
+            query_zipcode,
+            within_miles=query_distance,
+            specialization=query_specialization,
+            years_of_experience=query_years_experience,
+            insurence=query_insurance,
+            language=query_lang,
+            gender=query_gender,
+        )
 
         return query_results(docs)
-    
+
     if form.errors != {}:  # If there are errors from the validations
         for err_msg in form.errors.values():
-            flash(f'There was an error with creating a user: {err_msg}', category='danger')
+            flash(
+                f"There was an error with creating a user: {err_msg}", category="danger"
+            )
 
-    return render_template('docquery.html', form=form)
+    return render_template("docquery.html", form=form)
 
-@app.route('/query_results')
+
+@app.route("/query_results")
 def query_results(doctors):
-    return render_template('query_results.html', doctors = doctors)
+    return render_template("query_results.html", doctors=doctors)
 
-@app.route('/doctor/<doctor_id>')
+
+@app.route("/doctor/<doctor_id>")
 def doctor(doctor_id):
     form = TextMessageForm()
     doc = get_doc_by_id(doctor_id)
-    return render_template('doctor.html', form=form, doctor = doc)
+    return render_template("doctor.html", form=form, doctor=doc)
